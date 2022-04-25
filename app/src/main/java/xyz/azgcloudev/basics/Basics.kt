8222991b4e00c38dc0ebd6e0134b53e083cb041e
Aldair Zamora <aldair.zamora@outlook.com>
2022-04-24 22:18:41 -0600

    Initial commit

---
+++ app/src/main/java/xyz/azgcloudev/basics/Basics.kt
@@ -0,0 +1,14 @@
+package xyz.azgcloudev.basics
+
+fun main() {
+
+    // variable with var
+    var myName = "Aldair"
+    //reassign a variable
+    myName = "Antonio"
+
+    // print
+    print("hello kotlin " + myName)
+
+    
+}
