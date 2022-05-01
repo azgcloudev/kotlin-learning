d2612cae2d84020f0dbe99d23505c30d8d449a63
Aldair Zamora <aldair.zamora@outlook.com>
2022-04-30 18:08:38 -0600

    study day

---
+++ app/src/main/java/xyz/azgcloudev/basics/AssignmentIncrementDecrementOperators.kt
@@ -0,0 +1,17 @@
+package xyz.azgcloudev.basics
+
+fun main() {
+
+    // Assignment Operators  [ += -= *= /= %= ]
+    var number1 = 5
+    number1 += 4
+    println(number1) //result is: 9
+
+
+    // Increment and decrement [ ++, -- ]
+    // increase the value by one or decrease it by 1
+    println(number1) // 9
+    println(number1++) // 9; sum is execute at the end of the line
+    println(++number1) //11 // sum is execute in the same line
+
+}
---
+++ app/src/main/java/xyz/azgcloudev/basics/Comparison.kt
@@ -0,0 +1,8 @@
+package xyz.azgcloudev.basics
+
+// Comparison operators ==, !=, <, >, <=, >=
+
+fun main() {
+    val isEqual: Boolean = 6 == 3
+    println("'isEqual' is $isEqual")
+}
